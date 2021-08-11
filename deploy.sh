3aebce5e28255b717356a5223891c99672cf5ceb
Arthur Campbell <arfa.camble@gmail.com>
2021-08-11 18:09:07 +0100

    application

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:arfacamble/personalised-cv.git master:gh-pages
+git push -f git@github.com:arfacamble/fx-digital.git master:gh-pages
 cd -
